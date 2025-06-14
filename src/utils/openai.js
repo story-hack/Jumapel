13730098fdc2b4c4ee9202a84903aba10a34bc93
Ahmed <makindeahmed2110@gmail.com>
2025-06-14 12:30:32 +0100

    latest

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 
 const openai = new OpenAI({
   baseURL: 'https://openrouter.ai/api/v1',
-  apiKey: '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',
+  apiKey: 'process.env.OPENAI_API_KEY',
   defaultHeaders: {
     'HTTP-Referer': 'http://localhost:3000', // Optional. Site URL for rankings on openrouter.ai.
     'X-Title': 'Jumapel', // Optional. Site title for rankings on openrouter.ai.
